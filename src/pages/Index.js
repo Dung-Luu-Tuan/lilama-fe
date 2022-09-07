cb7e1622f6cf4ca74d572823a1f80f7a079c716d
luu tuan dung <luutuandung102@gmail.com>
2022-09-07 22:44:23 +0700

    new commit

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,27 +1,163 @@
 import Header1 from "../components/Header";
-import { Layout, Col, Row, Avatar } from "antd";
-import { UserOutlined, DownOutlined } from "@ant-design/icons";
+import { Col, Row, Avatar, Space, Table, Tag, Checkbox, Drawer } from "antd";
+
+import {
+  UserOutlined,
+  CaretDownOutlined,
+  MenuOutlined,
+  CloseOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
+
+const onChange = (e) => {
+  console.log(`checked = ${e.target.checked}`);
+};
+
+const columns = [
+  {
+    title: "Year_HDK",
+    dataIndex: "year_hdk",
+    key: "year_hdk",
+    render(data) {
+      return <Checkbox onChange={onChange}>{data}</Checkbox>;
+    },
+  },
+  {
+    title: "Month_HDK",
+    dataIndex: "month_hdk",
+    key: "month_hdk",
+    render(data) {
+      return <Checkbox onChange={onChange}>{data}</Checkbox>;
+    },
+  },
+  {
+    title: "CBDA",
+    dataIndex: "cbda",
+    key: "cbda",
+    render(data) {
+      return <Checkbox onChange={onChange}>{data}</Checkbox>;
+    },
+  },
+  {
+    title: "Project",
+    key: "project",
+    dataIndex: "project",
+    render(data) {
+      return <Checkbox onChange={onChange}>{data}</Checkbox>;
+    },
+  },
+  {
+    title: "HDC_no",
+    key: "hdc_no",
+    dataIndex: "hdc_no",
+    render(data) {
+      return <Checkbox onChange={onChange}>{data}</Checkbox>;
+    },
+  },
+  {
+    title: "HDK_no",
+    key: "hdk_no",
+    dataIndex: "hdk_no",
+    render(data) {
+      return <Checkbox onChange={onChange}>{data}</Checkbox>;
+    },
+  },
+  {
+    title: "DVTT",
+    key: "dvtt",
+    dataIndex: "dvtt",
+    render(data) {
+      return <Checkbox onChange={onChange}>{data}</Checkbox>;
+    },
+  },
+  {
+    title: "Contractor",
+    key: "contractor",
+    dataIndex: "contractor",
+    render(data) {
+      return <Checkbox onChange={onChange}>{data}</Checkbox>;
+    },
+  },
+];
+
+const dataSource = [
+  {
+    key: "1",
+    year_hdk: "(All)",
+    month_hdk: "(All)",
+    cbda: "(All)asdddddddddddddddd",
+    project: "(All)",
+    hdc_no: "(All)",
+    hdk_no: "(All)",
+    dvtt: "(All)",
+    contractor: "(All)",
+  },
+  {
+    key: "2",
+    year_hdk: "(All)",
+    month_hdk: "(All)",
+    cbda: "(All)",
+    project: "(All)",
+    hdc_no: "(All)",
+    hdk_no: "(All)",
+    dvtt: "(All)",
+    contractor: "(All)",
+  },
+];
 
 function Index() {
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <Row>
-        <Col span={20} push={4} className="bg-blue-600 h-screen">
-          <Row
-            className="bg-blue-400"
-            justify="space-between items-center h-12 px-2"
-          >
-            <Col>col-8</Col>
+        <Col span={24} className="main">
+          <Row className="header">
             <Col>
-              <Avatar icon={<UserOutlined />} className="mr-8" /> col-8
-              <DownOutlined className="text-yellow-400" />
+              <MenuOutlined
+                className="header_menuOutLined"
+                onClick={showDrawer}
+              />
+            </Col>
+            <Col>
+              <Avatar icon={<UserOutlined />} className="header__avatar" />
+              col-8
+              <CaretDownOutlined className="header__dropdownIcon" />
             </Col>
           </Row>
-          Container
+          <Row className="container">
+            <Row className="dashboard">
+              <Row className="dashboard__title">Dashboard</Row>
+              <Table
+                columns={columns}
+                dataSource={dataSource}
+                pagination={false}
+                className="dashboard__table"
+              />
+            </Row>
+          </Row>
         </Col>
-        <Col span={4} pull={20} className="bg-red-600 h-screen">
-          Menu
-        </Col>
+        <Drawer
+          title="Basic Drawer"
+          placement="left"
+          onClose={onClose}
+          open={open}
+          closeIcon={
+            <CloseOutlined style={{ flexDirection: "row !important" }} />
+          }
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
       </Row>
     </>
   );
