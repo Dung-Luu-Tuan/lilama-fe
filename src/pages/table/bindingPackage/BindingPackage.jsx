1f55eee537a2e1bbf56606101485d0dde2c3ca1c
Tang Minh Tien <tiengiang951753@gmail.com>
2022-09-13 21:45:53 +0700

    bindingPackage

--- src/pages/table/bindingPackage/BindingPackage.jsx
+++ src/pages/table/bindingPackage/BindingPackage.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import "../datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { bindingPackageColumns } from "./Columns";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const BindingPackage = () => {
-  return <div>BindingPackagedasdasd</div>;
-};
+  const [data, setData] = useState([]);
 
+  // useEffect(() => {
+  //   axios
+  //     .get("https://lilama18.herokuapp.com/api/projects?page=1&limit=20", {
+  //       headers: { Authorization: window.localStorage.getItem("token") },
+  //     })
+  //     .then((response) => setData(response.data.data));
+  // }, []);
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <div className="viewButton">Edit</div>
+            <div className="deleteButton">Disable</div>
+          </div>
+        );
+      },
+    },
+  ];
+  const dataTest = [
+    {
+      code: "001",
+      description: "Gói thầu 1"
+    },
+    {
+      code: "002",
+      description: "Gói thầu 2"
+    },
+    {
+      code: "003",
+      description: "Gói thầu 3"
+    }
+  ]
+  return (
+    <div className="datatable">
+      <div className="datatableTitle">
+        Thêm dự án mới
+        <Link to="/project/projectUpload" className="link">
+          Thêm mới
+        </Link>
+      </div>
+      <DataGrid
+        className="datagrid"
+        rows={dataTest}
+        columns={bindingPackageColumns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        checkboxSelection
+        getRowId={(row) => row.code}
+      />
+    </div>
+  );
+
+};
 export default BindingPackage;
---
+++ src/pages/table/bindingPackage/Columns.jsx
@@ -0,0 +1,12 @@
+export const bindingPackageColumns = [
+    {
+        field: "code",
+        headerName: "Tên gói thầu",
+        width: 400,
+    },
+    {
+        field: "description",
+        headerName: "Thông tin gói thầu",
+        width: 700,
+    }
+]
